9688c2d671aed5ff8177d4381a710860e02b64ef
Mukul <mukujjee@gmail.com>
2022-09-16 20:55:10 +0530

    Full build

--- src/js/script.js
+++ src/js/script.js
@@ -55,3 +55,60 @@ hamburger.addEventListener("click", () =>{
     navMenu.classList.toggle("active");
     body.classList.toggle("hamburger-active");
 })
+
+//================timeline===================
+const timelineName = document.querySelectorAll(".timeline ul li a");
+const timeline = document.querySelectorAll(".timeline ul li");
+const timeRadio = document.querySelectorAll(".timeline ul li::before");
+const timeText = document.getElementById("time--text");
+const timeListOne = document.getElementById("time-list--one");
+const timeListTwo = document.getElementById("time-list--two");
+const timeListThree = document.getElementById("time-list--three");
+const timeListFour = document.getElementById("time-list--four");
+
+function timelineClick(){
+    for(let i = 0; i < timeline.length; i++){
+        timeline[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+timeline.forEach(item =>{
+    item.addEventListener("click", timelineClick);
+});
+
+timeListOne.addEventListener("click", ()=>{
+    timeText.innerHTML = `<h2 class="text--secondary color--black-two margin-bottom--50">Timeline</h2>
+    <p class="text--quarternary color--gray margin-bottom--30">A posuere scelerisque gravida facilisi parturient magna urna donec parturient per augue a amet quam nullam a ad a. Parturient velit imperdiet in suspendisse eget a parturient adipiscing penatibus convallis himenaeos felis torquent facilisi elementum a penatibus suscipit aliquam.</p>
+    <p class="text--quarternary color--gray">Porta at at suspendisse dolor fames ullamcorper cum id per suspendisse a dignissim eu dapibus platea turpis augue mi proin lorem morbi sapien facilisis lectus.</p>`;
+});
+
+timeListTwo.addEventListener("click", ()=>{
+    timeText.innerHTML = `<h2 class="text--secondary color--black-two margin-bottom--50">Important Date One</h2>
+    <p class="text--quarternary color--gray margin-bottom--30">A posuere scelerisque gravida facilisi parturient magna urna donec parturient per augue a amet quam nullam a ad a. Parturient velit imperdiet in suspendisse eget a parturient adipiscing penatibus</p>
+    <p class="text--quarternary color--gray">Porta at at suspendisse dolor fames ullamcorper cum id per suspendisse a dignissim eu dapibus.</p>`;
+});
+
+timeListThree.addEventListener("click", ()=>{
+    timeText.innerHTML = `<h2 class="text--secondary color--black-two margin-bottom--50">Important Date Two</h2>
+    <p class="text--quarternary color--gray margin-bottom--30">A posuere scelerisque gravida facilisi parturient magna urna donec parturient per augue a amet quam nullam a ad a. Parturient velit imperdiet in suspendisse eget a parturient adipiscing penatibus convallis himenaeos felis torquent facilisi elementum a penatibus suscipit aliquam. himenaeos felis torquent facilisi elementum a penatibus suscipit aliquam.</p>
+    <p class="text--quarternary color--gray">Porta at at suspendisse dolor fames ullamcorper cum id per suspendisse a dignissim eu dapibus platea turpis augue mi proin lorem morbi sapien facilisis lectus.himenaeos felis torquent facilisi elementum a penatibus suscipit aliquam.</p>`;
+});
+timeListFour.addEventListener("click", ()=>{
+    timeText.innerHTML = `<h2 class="text--secondary color--black-two margin-bottom--50">Current Time</h2>
+    <p class="text--quarternary color--gray margin-bottom--30">A posuere scelerisque gravida facilisi parturient magna urna donec parturient per augue a amet quam nullam a ad a. Parturient velit imperdiet in suspendisse eget a parturient adipiscing penatibus convallis himenaeos felis torquent facilisi elementum a penatibus suscipit aliquam.</p>`;
+});
+
+//==============navmenu items click====================
+
+const navItems = document.querySelectorAll("header ul li");
+
+navItems.forEach(item =>{
+    item.addEventListener("click", () =>{
+        for(let i = 0; i < navItems.length; i++){
+            navItems[i].classList.remove("active");
+        }
+        item.classList.add("active");
+    })
+})
+
